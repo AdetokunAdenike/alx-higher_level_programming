500d6fb8b5e447b89828c36068661f2b2b615d9e
AdetokunAdenike <adetokunadenike@gmail.com>
2024-03-13 22:59:11 +0100

    Update

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H_
-#define LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 /**
@@ -21,5 +21,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif /* LISTS_H_ */
+#endif /* LISTS_H */
 
