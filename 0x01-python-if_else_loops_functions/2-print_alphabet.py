10b9863b17048461bebb4cc96b3ecf77b3dda5b2
AdetokunAdenike <adetokunadenike@gmail.com>
2024-03-13 23:45:38 +0100

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-import string
-
-lowercase_alphabet = string.ascii_lowercase
-
-print(lowercase_alphabet, end='')
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='')
