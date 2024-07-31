5f8407c4d9fe0c5f999856a531335f03114911c6
AdetokunAdenike <adetokunadenike@gmail.com>
2024-07-30 19:46:35 -0700

    web scrapping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,6 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
 
-const targetUrl = process.argv[2];
-const outputFilePath = process.argv[3];
-
-request(targetUrl)
-  .pipe(fs.createWriteStream(outputFilePath));
+request(process.argv[2])
+  .pipe(fs.createWriteStream(process.argv[3]));
