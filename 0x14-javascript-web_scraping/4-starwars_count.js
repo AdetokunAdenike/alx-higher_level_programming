937f3fcffdd0813c62a8a56fe25a9d286e1d14a8
AdetokunAdenike <adetokunadenike@gmail.com>
2024-07-30 19:40:09 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,12 +6,12 @@ request(process.argv[2], function (error, response, body) {
     console.error('Error:', error);
     return;
   }
-
+  
   const results = JSON.parse(body).results;
 
-   const count = results.reduce((acc, movie) => {
+  const count = results.reduce((acc, movie) => {
     return movie.characters.some(character => character.endsWith('/18/')) ? acc + 1 : acc;
   }, 0);
 
-   console.log(count);
+  console.log(count);
 });
