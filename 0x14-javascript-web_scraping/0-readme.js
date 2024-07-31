afe2c27fc7926e69934ffcc71f74690be8f16c28
AdetokunAdenike <adetokunadenike@gmail.com>
2024-07-30 18:06:59 -0700

    web scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (error, content)
-	{
-	if (error)
-		{
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+	if (error) {
 		console.error(error);
 		}
-	else
-		{
+	else {
 		console.log(content);
 		}
-	});
+});
