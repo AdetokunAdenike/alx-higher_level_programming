284d060d495b11b13f79a5c2a45bce357cb207ef
AdetokunAdenike <adetokunadenike@gmail.com>
2024-07-30 19:54:49 -0700

    web scrapping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,34 +2,22 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const movieApiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode === 200) {
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-
-    characterUrls.forEach((characterUrl, index) => {
-      request(characterUrl, (err, res, characterBody) => {
-        if (err) {
-          console.error('Error:', err);
-          return;
-        }
-
-        if (res.statusCode === 200) {
-          const characterData = JSON.parse(characterBody);
-          console.log(characterData.name);
-        } else {
-          console.error('Error code:', res.statusCode);
-        }
-      });
-    });
-  } else {
-    console.error('Error code:', response.statusCode);
+request(movieApiUrl, (err, res, data) => {
+  if (!err) {
+    const characterUrls = JSON.parse(data).characters;
+    fetchCharacterNames(characterUrls, 0);
   }
 });
+
+function fetchCharacterNames(urls, position) {
+  request(urls[position], (err, res, characterData) => {
+    if (!err) {
+      console.log(JSON.parse(characterData).name);
+      if (position + 1 < urls.length) {
+        fetchCharacterNames(urls, position + 1);
+      }
+    }
+  });
+}
